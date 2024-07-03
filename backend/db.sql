fb7a81fc7b7ab6d1bc32454a3300310bd811714b
AbdoulayeXYZ <niasseabdoulaye64@gmail.com>
2024-07-03 21:10:13 +0000

    db.sql added

--- backend/db.sql
+++ backend/db.sql
@@ -18,12 +18,13 @@ INSERT INTO products (name, price, image, description) VALUES
 ('Caramel Cream Delight', 4500, 'https://readymadeui.com/images/coffee3.webp', 'Un mélange onctueux de café doux et de caramel sucré, couronné une généreuse dose de crème fouettée.'),
 ('Hazelnut Heaven Blend', 4800, 'https://readymadeui.com/images/coffee4.webp', "Un café aromatique infusé avec la saveur riche et chaleureuse des noisettes. Une expérience gustative réconfortante."),
 ('Vanilla Velvet Brew', 4700, 'https://readymadeui.com/images/coffee5.webp', " Un café soyeux enrichi une touche de vanille douce et crémeuse. Une boisson élégante et raffinée."),
-('Double Shot Symphony', 5200, 'https://readymadeui.com/images/coffee6.webp', " Pour les amateurs de café fort, ce double espresso offre une symphonie de saveurs intenses et un boost énergie.'),
+('Double Shot Symphony', 5200, 'https://readymadeui.com/images/coffee6.webp', " Pour les amateurs de café fort, ce double espresso offre une symphonie de saveurs intenses et un boost énergie."),
 ('Irish Cream Dream', 5500, 'https://readymadeui.com/images/coffee7.webp', 'Un mélange harmonieux de café et de crème irlandaise, offrant une expérience gustative douce et légèrement alcoolisée.'),
 ('Coconut Bliss Coffee', 5300, 'https://readymadeui.com/images/coffee8.webp', 'Une création tropicale unique, combinant le goût riche du café avec la douceur exotique de la noix de coco.'),
 ('Espresso Elegante', 4000, 'https://readymadeui.com/images/coffee1.webp', 'Notre espresso signature, offrant une expérience gustative intense et raffinée avec des notes de chocolat noir et de noisettes grillées.');
 
 -- Création de la table sales
+
 CREATE TABLE IF NOT EXISTS sales (
     id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT NOT NULL,
